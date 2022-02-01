0b5360eb1098d3da14384d2c8357bdc79df317cb
Skaushal2 <32886225+kaushalSuryansh12@users.noreply.github.com>
2022-02-01 10:25:11 +0530

    Add project files.

---
+++ Match_Maker_App/Profile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DatingProfile
+{
+
+    class Profile
+    {
+
+        // fields
+
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        // constructors
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+
+            this.name = name;
+            this.Age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[] { };
+
+        }
+
+        // age validation property
+
+        private int Age
+        {
+            get { return age; }
+            set
+            {
+                if ((value >= 18) && (value <= 58))
+                {
+                    age = value;
+                }
+            }
+        }
+
+
+        // methods
+
+        // view profile method
+        public string ViewProfile()
+        {
+
+            string hobbiesArray = "";
+            foreach (string element in this.hobbies)
+            {
+                hobbiesArray = hobbiesArray + "\n" + element;
+            }
+            string profileInfo = $"{name}\n{age}\n{city}\n{country}\n{pronouns}{hobbiesArray}";
+            return profileInfo;
+        }
+
+
+        // set hobbies method 
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+    }
+
+
+}
---
+++ Match_Maker_App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // constructor method being used for input
+            Profile sam = new Profile("Sam", 16, "New York", "America", "he/him");
+
+            // setting hobbies using hobby method
+            sam.SetHobbies(new string[] { "He likes to play soccer", "He likes to enjoy music" });
+
+            // view profile method 
+            string profileInfo = sam.ViewProfile();
+            Console.WriteLine(profileInfo);
+
+        }
+    }
+}
